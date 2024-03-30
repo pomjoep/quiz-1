7aee22cf3ea598e23b9da8f62e18c6338c800c06
pomjoep <59618718+pomjoep@users.noreply.github.com>
2024-03-29 17:24:21 -0700

    Create Exercise2.java

---
+++ Exercise2.java
@@ -0,0 +1,19 @@
+package Exercise;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        exercise2();
+    }
+
+    private static void exercise2() {
+        /*
+        Task: check whether a list of integer is power of 2
+        Input: a list of integers
+        Output: true or false
+        Logic: return TRUE if a given number is a power of 2, such as 2, 4, 8, and return FALSE otherwise
+        Validation: test your function up to at least 128   // What is testing? see https://youtu.be/u6QfIXgjwGQ?si=6_QCuitJwU1G6pjL
+        Additional improvement: make your code run as fast as possible
+         */
+        //TODO: add your code
+    }
+}
