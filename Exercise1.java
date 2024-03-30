340b4cc6844f40fbf55e0a3513b6764030ffb7e0
pomjoep <59618718+pomjoep@users.noreply.github.com>
2024-03-29 17:23:16 -0700

    Create Exercise1.java

---
+++ Exercise1.java
@@ -0,0 +1,20 @@
+package Exercise;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+
+        int x = 123, y = 567;
+
+        /*
+        Task: swap the values in two variables
+        Input: two Integer variables
+        Output: nothing
+        Logic: the values of the 2 variables should be exchanged
+        Validation: 5 test cases at least
+        Additional improvement: Is it possible to not use any other variable at all for the swap?
+         */
+        //TODO: add your code
+
+        System.out.println("x = " + x + ", y = " + y);
+    }
+}
