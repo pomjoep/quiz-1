c665c494532f24fd44de56b3ea53e338a32c9343
pomjoep <59618718+pomjoep@users.noreply.github.com>
2024-03-30 02:25:56 -0700

    3 done

--- Exercise3.java
+++ Exercise3.java
@@ -27,8 +27,6 @@ public class Exercise3 {
                 {1}, //false
                 {0, 2, 1, 2, 3}, //false
                 {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 611}, //false
-                // TODO: add a few more...
-
         };
 
         for (int[] array : arrays) {
